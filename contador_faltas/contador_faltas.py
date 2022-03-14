b4d4d971e26eb169b0468c5413ff3a43698ef339
Nogueira23 <jvictor.n.m23@gmil.com>
2022-03-14 09:39:56 -0300

    modalagem final para gerar um relatório

--- contador_faltas/contador_faltas.py
+++ contador_faltas/contador_faltas.py
@@ -4,20 +4,23 @@ faltas = 0
 print("Responda sempra 'y' para sim ou 'n' para não nas preguntas")
 
 #Desenvolvimento
-while True:
-    pergunta1 = input("Ainda há faltas para registrar? ")
-    if pergunta1 == "n":
-        porcentagem = (faltas/total)*100
-        if porcentagem < 25:
-            aux = 25 - porcentagem
-            print(f"Você faltou {porcentagem}% das aulas")
-            if aux < 0:
-                print(f"Você está reprovado por falta, sorry!")
-            else:
-                restante = int((aux/100)*total)
-                print(f"Você ainda pode faltar {restante} aulas!")
-            break
-    else:
-        pergunta2 = int(input("Quantas aulas você faltou? "))
-        faltas += pergunta2
+with open("relatorio.csv", "r+") as relatorio:
+    for line in relatorio:
+        pass
+    while True:
+        pergunta1 = input("Há faltas para registrar? ")
+        if pergunta1 == "n":
+            porcentagem = (faltas/total)*100
+            if porcentagem < 25:
+                aux = 25 - porcentagem
+                print(f"Você faltou {porcentagem}% das aulas")
+                if aux < 0:
+                    print(f"Você está reprovado por falta, sorry!")
+                else:
+                    restante = int((aux/100)*total)
+                    print(f"Você ainda pode faltar {restante} aulas!")
+                break
+        else:
+            pergunta2 = int(input("Quantas aulas você faltou? "))
+            faltas += pergunta2
 
