52f5d33a5ca78fd4f0a7195301da3c138089bc9c
Nogueira23 <jvictor.n.m23@gmil.com>
2022-03-14 11:13:04 -0300

    gerando relatório e remodelagem

--- contador_faltas/contador_faltas.py
+++ contador_faltas/contador_faltas.py
@@ -1,26 +1,44 @@
 #Entrada inicial
-total = int(input("Quantas aulas terá a discilina? "))
 faltas = 0
 print("Responda sempra 'y' para sim ou 'n' para não nas preguntas")
-
+datas = []
 #Desenvolvimento
-with open("relatorio.csv", "r+") as relatorio:
+with open("relatorio.csv", "r") as relatorio:
     for line in relatorio:
-        pass
+        linha = line.split(";")
+        if linha[0] == "total":
+            continue
+        else:
+            if linha[0] == '0':
+                total = int(input("Quantas aulas terá a discilina? "))
+            else:
+                total = int(linha[0])
+            faltas = int(linha[1])
+            porcentagem = float(linha[2])
+            restante = int(linha[3])
     while True:
         pergunta1 = input("Há faltas para registrar? ")
         if pergunta1 == "n":
-            porcentagem = (faltas/total)*100
-            if porcentagem < 25:
-                aux = 25 - porcentagem
-                print(f"Você faltou {porcentagem}% das aulas")
-                if aux < 0:
-                    print(f"Você está reprovado por falta, sorry!")
-                else:
-                    restante = int((aux/100)*total)
-                    print(f"Você ainda pode faltar {restante} aulas!")
-                break
+            aux = 25 - porcentagem
+            if aux < 0:
+                aux = 0
+            restante = int((aux/100)*total)
+            datas.append(total)
+            datas.append(faltas)
+            datas.append(porcentagem)
+            datas.append(restante)
+            break
         else:
             pergunta2 = int(input("Quantas aulas você faltou? "))
             faltas += pergunta2
+            porcentagem = float(((faltas/total))*100)
+#Atualização de relatório
+with open("relatorio.csv", "w") as relatorio:
+    head = f'total;faltas;faltas_porcentagem;faltas_restantes'
+    head += "\n"
+    relatorio.write(head)
+    line = f'{datas[0]};{datas[1]};{datas[2]}%;{datas[3]}'
+    line += "\n"
+    relatorio.write(line)
 
+            
